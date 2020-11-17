e72c4e29d868d374c1ad9f215c05b219e693ce61
zyang99 <1402687676@qq.com>
2020-11-17 16:07:04 +0800

    所有编程语言对浮点数据精确度很难确定

--- tttt.py
+++ tttt.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 
+x = 0.1
+y = 0.2
+print(x+y)    #0.30000000000000004
 
-print(tf.__version__)
-print(tf.__path__)
+#所有编程语言 对浮点型数据的精确度都很难确定
+# print(tf.__version__)
+# print(tf.__path__)
 
